d09c3346222087000d018b91dd7990573649cd2f
Johann Jacobsohn <j.jacobsohn@satzmedia.de>
2020-07-06 20:05:46 +0200

    add new Packaging stage to ci

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='gascamcontrol',
+    version='0.0.1',
+    description='',
+    author='Johann Jacobsohn',
+    author_email='johann.jacobsohn@uni-hamburg.de',
+    package_dir={'gascamcontrol': 'gascamcontrol'},
+    packages=['gascamcontrol', 'tests'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
